b11cb4078640caf314fe49f5570250e868395850
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-03-26 20:37:51 +0100

    Add 7.2 tagClickHandler / addClickListenersToTags

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Generate TITLE CLICK HANDLER
+
 const titleClickHandler = function (event) {
   event.preventDefault();
 
@@ -48,7 +50,10 @@ const
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list';
 
-function generateTitleLinks() {
+
+// Function GENERATE TITLE LINKS
+
+function generateTitleLinks(customSelector = '') {
 
   /* remove contents of titleList */
 
@@ -56,7 +61,6 @@ function generateTitleLinks() {
   // zawarte w stałem optTitleListSelector
   // titleList = optTitleListSelector = .titles
   const titleList = document.querySelector(optTitleListSelector);
-
 
   titleList.innerHTML = '';
   // console.log('wyczyszczona lista artykułów: ', titleList);
@@ -66,7 +70,9 @@ function generateTitleLinks() {
   // do stałej articles wyszukujemy i przypisujemy wszystkie wyszukane
   // elementy zawarte w stałej optArticleSelector
   // articles = optArticleSelector = .post
-  const articles = document.querySelectorAll(optArticleSelector);
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+  // console.log('wyświetlam custom selector: ', customSelector);
+  // console.log('wyświetlam custom optArticleSelector: ', optArticleSelector + customSelector);
 
   let html = '';
 
@@ -117,6 +123,7 @@ function generateTitleLinks() {
   }
 }
 
+// Function GENERATE TAGS
 
 function generateTags() {
   /* find all articles */
@@ -146,7 +153,7 @@ function generateTags() {
 
       /* generate HTML of the link */
       const tagLinkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
-      console.log('wyświetla link do każdego taga: ', tagLinkHTML);
+      // console.log('wyświetla link do każdego taga: ', tagLinkHTML);
 
       /* add generated code to html variable */
       html += tagLinkHTML;
@@ -160,5 +167,65 @@ function generateTags() {
   }
 }
 
+// Function TAG CLICK HANDLER
+
+function tagClickHandler(event) {
+
+  /* prevent default action for this event */
+  event.preventDefault();
+
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this;
+
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+
+  /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
+
+  /* find all tag links with class active */
+  const tagLinksActiveClass = document.querySelectorAll('a.active[href^="#tag-"]');
+  // podpowiedź
+
+  /* START LOOP: for each active tag link */
+  for (let tagLinkActiveClass of tagLinksActiveClass) {
+
+    /* remove class active */
+    tagLinkActiveClass.classList.remove('active');
+  }
+  /* END LOOP: for each active tag link */
+
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  const tagLinksHref = document.querySelectorAll('a[href="' + href + '"]');
+
+  /* START LOOP: for each found tag link */
+  for (let tagLinkHref of tagLinksHref) {
+
+    /* add class active */
+    tagLinkHref.classList.add('active');
+
+    /* END LOOP: for each found tag link */
+  }
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks('[data-tags~="' + tag + '"]');
+}
+
+function addClickListenersToTags() {
+  /* find all links to tags */
+
+  const tagLinks = document.querySelectorAll('a.active[href^="#tag-"]');
+
+  /* START LOOP: for each link */
+  for (let tagLink of tagLinks) {
+
+    /* add tagClickHandler as event listener for that link */
+    tagLink.addEventListener('click', tagClickHandler);
+
+    /* END LOOP: for each link */
+  }
+}
+
+addClickListenersToTags();
 generateTitleLinks();
 generateTags();
